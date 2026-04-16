e23e725c2a00c082b00d692a42a03d512f5a4c97
Richard Ressler <rressler@american.edu>
2026-04-16 19:34:34 -0400

    add lint

--- app.R
+++ app.R
@@ -17,12 +17,7 @@ ui <- fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-        "Number of bins:",
-        min = 1,
-        max = 50,
-        value = 30
-      )
+      sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30)
     ),
 
     # Show a plot of the generated distribution
@@ -40,8 +35,11 @@ server <- function(input, output) {
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
     # draw the histogram with the specified number of bins
-    hist(x,
-      breaks = bins, col = "darkgray", border = "white",
+    hist(
+      x,
+      breaks = bins,
+      col = "darkgray",
+      border = "white",
       xlab = "Waiting time to next eruption (in mins)",
       main = "Histogram of waiting times"
     )
